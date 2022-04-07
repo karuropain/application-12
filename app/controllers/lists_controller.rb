2a5d43ff0bd2af30cb4fec4ba05bab351fa9e41c
karuropain <fs.sai5041@gmail.com>
2022-04-07 14:16:42 +0000

    Update テストのみ

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,6 +6,7 @@ class ListsController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to list_path(@book.id)
     else
       @books = Book.all
@@ -25,6 +26,7 @@ class ListsController < ApplicationController
   def destroy 
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
   
@@ -35,6 +37,7 @@ class ListsController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
       redirect_to list_path(@book.id)
     else
       render :edit
