5bdeef495768c75072f1f26f8a55dfca84d83861
karuropain <fs.sai5041@gmail.com>
2022-04-10 02:53:41 +0000

    Update テスト設定完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,53 @@
 class BooksController < ApplicationController
-  def index
-  end
-
   def new
+    @book = Book.new
   end
   
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to list_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
   
-  def show
+  def index
+    @book = Book.new
+    @books = Book.all
   end
 
-  def edit
+  def show
+    @book = Book.find(params[:id])
   end
+  
+  def destroy 
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to list_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
